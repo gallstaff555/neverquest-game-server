5264af04b125f96805cbce3f78f97888157cfe13
David Solomon <david@mac-mini.mynetworksettings.com>
2024-12-15 23:29:37 -0800

    Create new dockerfiles for EC2 env

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 sudo docker build -t neverquest-account-server:latest -f docker/account-server/Dockerfile .
 sudo docker build -t neverquest-game-server:latest -f docker/game-server/Dockerfile .
+
+#sudo docker build -t neverquest-account-server:latest -f docker/account-server/local_Dockerfile .
+#sudo docker build -t neverquest-game-server:latest -f docker/game-server/local_Dockerfile .
